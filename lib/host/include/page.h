6c4ac57fb1eeb87050d64a2c649c6cbbe3b25a8c
Alex Thomas <alexthomas@berkeley.edu>
2019-07-08 22:48:17 -0700

    Debug

--- lib/host/include/page.h
+++ lib/host/include/page.h
@@ -104,19 +104,24 @@ static pte_t* __ept_continue_walk_create(vaddr_t base_addr, vaddr_t *pg_list, pt
 	unsigned long free_ppn = ppn(*pg_list);
 	*pte = ptd_create(free_ppn);
   *pg_list += PAGE_SIZE;
-//	printf("ptd_create: ppn = %p, pte = %p\n", (void *) (free_ppn << RISCV_PGSHIFT), (void *) (*pte).pte);
-	return __ept_walk_create(base_addr, pg_list, root_page_table, addr, fd, hash);
+  //	printf("ptd_create: ppn = %p, pte = %p\n", (void *) (free_ppn << RISCV_PGSHIFT), (void *) (*pte).pte);
+  return __ept_walk_create(base_addr, pg_list, root_page_table, addr, fd, hash);
+  
+//  if(!hash)
+//	  return __ept_walk_create(base_addr, pg_list, root_page_table, addr, fd, hash);
+//  else
+//    return __ept_walk_create(base_addr, pg_list, root_page_table, addr, fd, hash);
 }
 
-static pte_t* __ept_continue_walk_create_hash(vaddr_t base_addr, vaddr_t *pg_list, pte_t* root_page_table, vaddr_t addr, pte_t* pte, int fd)
-{
-  //Gets free page list from pg_list
-  unsigned long free_ppn = ppn(*pg_list);
-  *pte = ptd_create(free_ppn);
-  *pg_list += PAGE_SIZE;
-//	printf("ptd_create: ppn = %p, pte = %p\n", (void *) (free_ppn << RISCV_PGSHIFT), (void *) (*pte).pte);
-  return __ept_walk_create_hash(base_addr, pg_list, root_page_table, addr, fd);
-}
+//static pte_t* __ept_continue_walk_create_hash(vaddr_t base_addr, vaddr_t *pg_list, pte_t* root_page_table, vaddr_t addr, pte_t* pte, int fd)
+//{
+//  //Gets free page list from pg_list
+//  unsigned long free_ppn = ppn(*pg_list);
+//  *pte = ptd_create(free_ppn);
+//  *pg_list += PAGE_SIZE;
+////	printf("ptd_create: ppn = %p, pte = %p\n", (void *) (free_ppn << RISCV_PGSHIFT), (void *) (*pte).pte);
+//  return __ept_walk_create_hash(base_addr, pg_list, root_page_table, addr, fd);
+//}
 
 static pte_t* __ept_walk_internal(vaddr_t base_addr, vaddr_t* pg_list, pte_t* root_page_table, vaddr_t addr, int create, int fd)
 {
