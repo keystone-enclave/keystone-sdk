52acc7f58ab4dd233007efcd210dc3234e14bc96
Alex Thomas <alexthomas@berkeley.edu>
2019-07-10 22:27:20 -0700

    Debug

--- lib/host/src/keystone.cpp
+++ lib/host/src/keystone.cpp
@@ -518,12 +518,13 @@ keystone_status_t Keystone::measure(const char *eapppath, const char *runtimepat
   hash_enclave.untrusted_ptr = enclp.params.untrusted_ptr;
   hash_enclave.untrusted_size = enclp.params.untrusted_size;
 
-  printf("utm_size: %llu, epm_size: %llu, epm_paddr: %p, ut_ptr: %p, ut_sz: %llu, free-paddr: %p, utm_paddr: %p\n", hash_enclave.utm_size,
-          hash_enclave.epm_size,
+  printf("epm_end: %llu, epm_paddr: %p, ut_ptr: %p, ut_end: %p, free-paddr: %p, utm_size: %p, utm_paddr: %p\n",
+         (void *) (hash_enclave.epm_paddr + hash_enclave.epm_size),
          (void*) hash_enclave.epm_paddr,
          (void *) hash_enclave.untrusted_ptr,
-         hash_enclave.untrusted_size,
+         (void *) (hash_enclave.untrusted_ptr + hash_enclave.untrusted_size),
          (void *) hash_enclave.free_paddr,
+         (void *) (hash_enclave.utm_paddr + hash_enclave.utm_size),
          (void *) hash_enclave.utm_paddr);
 
   validate_and_hash_enclave(enclp.params, &hash_enclave);
