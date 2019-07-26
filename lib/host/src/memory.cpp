3b5ce511fc3db2727cfc19eb4c2b0a0efda54df6
Alex Thomas <alexthomas@berkeley.edu>
2019-07-26 00:04:47 -0700

    Debug

--- lib/host/src/memory.cpp
+++ lib/host/src/memory.cpp
@@ -32,7 +32,7 @@ vaddr_t Memory::AllocMem(bool is_phys, size_t size){
 
   vaddr_t ret;
   if(is_phys) {
-    ret = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    ret = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, keystone_fd, 0);
   }
   else{
     ret = allocate_aligned(size, PAGE_SIZE);
@@ -44,7 +44,7 @@ vaddr_t Memory::AllocMem(bool is_phys, size_t size){
 void Memory::Read(bool is_phys, vaddr_t src, vaddr_t buf, size_t size){
 
   if(is_phys) {
-    vaddr_t va_dst = (vaddr_t) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, dst - start_phys_addr);
+    vaddr_t va_dst = (vaddr_t) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, keystone_fd, dst - start_phys_addr);
     memcpy((void *) buf, (void *) src, size);
   }
   else{
@@ -56,7 +56,7 @@ void Memory::Read(bool is_phys, vaddr_t src, vaddr_t buf, size_t size){
 void Memory::Write(bool is_phys, vaddr_t src, vaddr_t dst, size_t size){
 
   if(is_phys) {
-    vaddr_t va_dst = (vaddr_t) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, dst - start_phys_addr);
+    vaddr_t va_dst = (vaddr_t) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, keystone_fd, dst - start_phys_addr);
     memcpy((void *) va_dst, (void *) src, size);
   }
   else{
