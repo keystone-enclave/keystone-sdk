932f3744b5f2594f2edccd27d35ef23b2cc30595
Catherine Lu <cathylu@berkeley.edu>
2021-05-01 19:02:15 +0000

    Testing with sqlite

--- examples/hello/eapp/hello.c
+++ examples/hello/eapp/hello.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
+#include "sqlite3.h"
 
 #define SYSCALL_SNAPSHOT 1005
 
@@ -34,11 +35,51 @@ int sbi_enclave_snapshot() {
   return SYSCALL_0(SYSCALL_SNAPSHOT);
 }
 
+int callback(void *NotUsed, int argc, char **argv, 
+                    char **azColName) {
+    
+    NotUsed = 0;
+    
+    for (int i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
 int main()
 {
   int size = 16;
   char * buf = (char*) malloc(1024*1024*size);
   unsigned long cycle_start, cycle_end;
+
+  sqlite3 *db; 
+  char* err_msg = 0;
+
+  int rc = sqlite3_open("chinook.db", &db);
+  if (rc != SQLITE_OK) {
+    printf("Cannot open database: %s\n", sqlite3_errmsg(db)); 
+    sqlite3_close(db);
+    return 1; 
+  }
+
+  char *query = "SELECT * from employees LIMIT 5"; 
+
+  rc = sqlite3_exec(db, query, callback, 0, &err_msg);
+  if (rc != SQLITE_OK ) {
+        
+        printf("SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        
+        return 1;
+  } 
+  
+  sqlite3_close(db);
 
   //*buf = 0x10;
   asm volatile("rdcycle %0" : "=r"(cycle_start));
