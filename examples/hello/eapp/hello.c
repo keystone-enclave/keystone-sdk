e7f48a84d48dabd9630f2b8f49163bf2ed955922
Catherine Lu <cathylu@berkeley.edu>
2021-05-01 19:02:15 +0000

    Testing with sqlite

--- examples/hello/eapp/hello.c
+++ examples/hello/eapp/hello.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int callback(void *NotUsed, int argc, char **argv, 
+                    char **azColName) {
+    
+    NotUsed = 0;
+    
+    for (int i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
 int main()
 {
   printf("hello, world!\n");
