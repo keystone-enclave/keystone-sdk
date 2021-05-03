9d80bae64154741575ceefc02b504d1efb0b1a10
Catherine Lu <cathylu@berkeley.edu>
2021-05-03 07:17:05 +0000

    Fixed bugs with getcwd

--- examples/hello/eapp/hello.c
+++ examples/hello/eapp/hello.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 
-int callback(void *NotUsed, int argc, char **argv, 
-                    char **azColName) {
-    
-    NotUsed = 0;
-    
-    for (int i = 0; i < argc; i++) {
-
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    
-    printf("\n");
-    
-    return 0;
-}
-
 int main()
 {
+  printf("hello, world!\n");
+  return 0;
+}
