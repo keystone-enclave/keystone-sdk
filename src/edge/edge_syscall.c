f726db054e07e5b79c71a52868c7f07b6c040f69
Catherine Lu <cathylu@berkeley.edu>
2021-05-03 07:21:56 +0000

    Adding database

--- src/edge/edge_syscall.c
+++ src/edge/edge_syscall.c
@@ -51,7 +51,7 @@ incoming_syscall(struct edge_call* edge_call) {
 			ret = fstatat(
           fstatat_args->dirfd, fstatat_args->pathname, &fstatat_args->stats,
           fstatat_args->flags);
-      perror("Finished running fstatat\n");
+      perror("Finished running fstatat");
 			printf("Return value: %ld\n", ret);
 			break;
     case (SYS_fstat):; 
