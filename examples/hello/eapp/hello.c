9a2507cfd73088c193581ab20162eee6ab8878dc
Catherine Lu <cathylu@berkeley.edu>
2021-05-05 07:04:56 +0000

    Add snapshot to sqlite test

--- examples/hello/eapp/hello.c
+++ examples/hello/eapp/hello.c
@@ -1,7 +1,8 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stddef.h>
+
 #include "sqlite3.h"
 
 #define SYSCALL_SNAPSHOT 1005
@@ -31,65 +32,98 @@
 #define SYSCALL_5(which, arg0, arg1, arg2, arg3, arg4) \
   SYSCALL(which, arg0, arg1, arg2, arg3, arg4)
 
-int sbi_enclave_snapshot() {
+int
+sbi_enclave_snapshot() {
   return SYSCALL_0(SYSCALL_SNAPSHOT);
 }
 
-int callback(void *NotUsed, int argc, char **argv, 
-                    char **azColName) {
-    
-    NotUsed = 0;
-    
-    for (int i = 0; i < argc; i++) {
+int
+callback(void* NotUsed, int argc, char** argv, char** azColName) {
+  NotUsed = 0;
 
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    
-    printf("\n");
-    
-    return 0;
-}
-
-int main()
-{
-  // int size = 16;
-  // char * buf = (char*) malloc(1024*1024*size);
-  // unsigned long cycle_start, cycle_end;
-
-  sqlite3 *db; 
-  char* err_msg = 0;
-
-  int rc = sqlite3_open("./chinook.db", &db);
-
-  printf("Return code: %d\r\n", rc);
- 
-   if (rc != SQLITE_OK) {
-    printf("Cannot open database: %s\n", sqlite3_errmsg(db)); 
-    sqlite3_close(db);
-    return 1; 
+  for (int i = 0; i < argc; i++) {
+    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
   }
 
-  char *query = "SELECT * from employees LIMIT 5"; 
+  printf("\n");
 
-  rc = sqlite3_exec(db, query, callback, 0, &err_msg);
-  if (rc != SQLITE_OK ) {
-        
-        printf("SQL error: %s\n", err_msg);
+  return 0;
+}
 
-        sqlite3_free(err_msg);
-        sqlite3_close(db);
-        
-        return 1;
-  } 
+int
+main() {
+  unsigned long cycle_start, cycle_end;
+
+  sqlite3 *fromFile, *inMemory;
+  char* err_msg = 0;
+  int rc;
+
+  rc = sqlite3_open("./chinook.db", &fromFile);
+  printf("Opened db from file: %d\r\n", rc);
+  if (rc != SQLITE_OK) {
+    printf("Cannot open database: %s\n", sqlite3_errmsg(fromFile));
+    sqlite3_close(fromFile);
+    return 1;
+  }
+
+  rc = sqlite3_open(":memory:", &inMemory);
+  printf("Opened in-memory db: %d\r\n", rc);
+  if (rc != SQLITE_OK) {
+    printf("Cannot open database: %s\n", sqlite3_errmsg(inMemory));
+    sqlite3_close(fromFile);
+    sqlite3_close(inMemory);
+    return 1;
+  }
+
+  sqlite3_backup* backup =
+      sqlite3_backup_init(inMemory, "main", fromFile, "main");
+  if (backup == NULL) {
+    printf("Failed to init backup %s\n", sqlite3_errmsg(inMemory));
+    sqlite3_close(fromFile);
+    sqlite3_close(inMemory);
+    return 1;
+  }
+
+  sqlite3_backup_step(backup, -1);
+  sqlite3_backup_finish(backup);
+  // rc = sqlite3_backup_step(backup, -1);
+  // if (rc != SQLITE_DONE) {
+  //   printf("Failed to backup: %s\n", sqlite3_errmsg(inMemory));
+  //   sqlite3_close(fromFile);
+  //   sqlite3_close(inMemory);
+  //   return 1;
+  // }
   
-  sqlite3_close(db);
+  // rc = sqlite3_backup_finish(backup);
+  // if (rc != SQLITE_DONE) {
+  //   printf("Failed to finish backup: %s\n", sqlite3_errmsg(inMemory));
+  //   sqlite3_close(fromFile);
+  //   sqlite3_close(inMemory);
+  //   return 1;
+  // }
 
-  // //*buf = 0x10;
-  // asm volatile("rdcycle %0" : "=r"(cycle_start));
-  // sbi_enclave_snapshot();
-  // asm volatile("rdcycle %0" : "=r"(cycle_end));
+  sqlite3_close(fromFile);
 
-  // //*buf = 0x2e;
-  // printf("%d, %ld\n", size, cycle_end - cycle_start);
+  sbi_enclave_snapshot();
+  char* query = "SELECT * from employees LIMIT 5";
+
+  asm volatile("rdcycle %0" : "=r"(cycle_start));
+  rc = sqlite3_exec(inMemory, query, callback, 0, &err_msg);
+  asm volatile("rdcycle %0" : "=r"(cycle_end));
+  if (rc != SQLITE_OK) {
+    printf("SQL error: %s\n", err_msg);
+
+    sqlite3_free(err_msg);
+    sqlite3_close(inMemory);
+
+    return 1;
+  }
+
+  printf(
+      "I need something really long so I can see it Cycles: %ld\n",
+      cycle_end - cycle_start);
+
+  sqlite3_close(inMemory);
+
   return 0;
 }
