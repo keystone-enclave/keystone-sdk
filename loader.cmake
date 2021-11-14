6e431ccc12b2b2fe52d24aa1639a52eb9284d0e0
Anay Wadhera <anay1018@gmail.com>
2021-11-14 00:43:51 +0000

    sdk: loader: relocate to top-level directory

--- loader.cmake
+++ loader.cmake
@@ -1,7 +1,7 @@
 set(LOADER_CFLAGS      "-c")
-set(LOADER_LDFLAGS     "-nostdlib -T${CMAKE_SOURCE_DIR}/src/loader/loader.lds")
-set(LOADER_SOURCE_DIR "${CMAKE_SOURCE_DIR}/src/loader")
-set(LOADER_OBJ_DIR    "${CMAKE_BINARY_DIR}/src/loader")
+set(LOADER_LDFLAGS     "-nostdlib -T${CMAKE_SOURCE_DIR}/loader/loader.lds")
+set(LOADER_SOURCE_DIR "${CMAKE_SOURCE_DIR}/loader/src")
+set(LOADER_OBJ_DIR    "${CMAKE_BINARY_DIR}/loader")
 
 list(APPEND ASM_SOURCE_FILES
     ${LOADER_SOURCE_DIR}/loader.S
@@ -17,8 +17,8 @@ list(APPEND C_SOURCE_FILES
     ${LOADER_SOURCE_DIR}/sbi.c
     ${LOADER_SOURCE_DIR}/string.c
     ${LOADER_SOURCE_DIR}/elf.c
-    ${LOADER_SOURCE_DIR}/../host/elf64.c
-    ${LOADER_SOURCE_DIR}/../host/elf32.c
+    ${CMAKE_SOURCE_DIR}/src/host/elf64.c
+    ${CMAKE_SOURCE_DIR}/src/host/elf32.c
     )
 
 list(APPEND C_OBJ_FILES
@@ -31,24 +31,20 @@ list(APPEND C_OBJ_FILES
     ${LOADER_OBJ_DIR}/elf32.c.o
     )
 
-set(INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/include/loader ${CMAKE_SOURCE_DIR}/include/host)
+set(INCLUDE_DIRS -I${CMAKE_SOURCE_DIR}/loader/inc -I${CMAKE_SOURCE_DIR}/include/host)
 
 set(CMAKE_C_FLAGS          "${CMAKE_C_FLAGS} ${CFLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LDFLAGS}")
 
-#include_directories(${INCLUDE_DIRS})
-
 # compile C files
 foreach (SOURCE_FILE OBJ_FILE IN ZIP_LISTS C_SOURCE_FILES C_OBJ_FILES)
-add_custom_command(OUTPUT ${OBJ_FILE} COMMAND riscv64-unknown-linux-gnu-gcc ${LOADER_CFLAGS} -I${CMAKE_SOURCE_DIR}/include/loader -I${CMAKE_SOURCE_DIR}/include/host ${SOURCE_FILE} -o ${OBJ_FILE})
+add_custom_command(OUTPUT ${OBJ_FILE} COMMAND riscv64-unknown-linux-gnu-gcc ${LOADER_CFLAGS} ${INCLUDE_DIRS} ${SOURCE_FILE} -o ${OBJ_FILE})
 endforeach()
 
 # compile ASM files
 foreach (SOURCE_FILE OBJ_FILE IN ZIP_LISTS ASM_SOURCE_FILES ASM_OBJ_FILES)
-add_custom_command(OUTPUT ${OBJ_FILE} COMMAND riscv64-unknown-linux-gnu-gcc ${LOADER_CFLAGS} -I${CMAKE_SOURCE_DIR}/include/loader -I${CMAKE_SOURCE_DIR}/include/host ${SOURCE_FILE} -o ${OBJ_FILE})
+add_custom_command(OUTPUT ${OBJ_FILE} COMMAND riscv64-unknown-linux-gnu-gcc ${LOADER_CFLAGS} ${INCLUDE_DIRS} ${SOURCE_FILE} -o ${OBJ_FILE})
 endforeach()
 
-add_custom_command(OUTPUT ${LOADER_OBJ_DIR}/loader.elf DEPENDS ${C_OBJ_FILES} DEPENDS ${ASM_OBJ_FILES} COMMAND riscv64-unknown-linux-gnu-ld ${LOADER_LDFLAGS} -I${CMAKE_SOURCE_DIR}/include/loader -I${CMAKE_SOURCE_DIR}/include/host ${C_OBJ_FILES} ${ASM_OBJ_FILES} -o ${LOADER_OBJ_DIR}/loader.elf)
+add_custom_command(OUTPUT ${LOADER_OBJ_DIR}/loader.elf DEPENDS ${C_OBJ_FILES} DEPENDS ${ASM_OBJ_FILES} COMMAND riscv64-unknown-linux-gnu-ld ${LOADER_LDFLAGS} ${INCLUDE_DIRS} ${C_OBJ_FILES} ${ASM_OBJ_FILES} -o ${LOADER_OBJ_DIR}/loader.elf)
 
-#install(FILES ${CMAKE_CURRENT_BINARY_DIR}/loader.bin DESTINATION ${out_dir}/bin)
-install(DIRECTORY ${INCLUDE_DIRS} DESTINATION ${out_dir}/include)
