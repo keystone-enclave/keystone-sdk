fd8759b3f13fee1888c910de703ba5712bdffbe0
Catherine Lu <cathylu@berkeley.edu>
2022-04-14 22:00:37 +0000

    Adding library enclave

---
+++ examples/library-enclave/host/host.cpp
@@ -0,0 +1,18 @@
+//******************************************************************************
+// Copyright (c) 2018, The Regents of the University of California (Regents).
+// All Rights Reserved. See LICENSE for license details.
+//------------------------------------------------------------------------------
+#include "edge/edge_call.h"
+#include "host/keystone.h"
+
+using namespace Keystone;
+
+int
+main(int argc, char** argv) {
+  Enclave enclave;
+  Params params;
+
+  enclave.initLibraryEnclave(argv[1], params);
+
+  return 0;
+}
